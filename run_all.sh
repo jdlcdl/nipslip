3c683a1f23620f15c3ef656389f3beb1631673e2
Jean Do <jean.dominique.lecomte.delisle@protonmail.me>
2024-12-06 16:07:43 -0400

    ALL defines ordered list of modules expected to be "working"

--- run_all.sh
+++ run_all.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
+ALL="bip340.py client.py constants.py mock_client.py mock.py nip01.py nip02.py nip04.py nip06.py nip09.py nip10.py nip13.py nip14.py nip19.py output.py relay.py "
 
-for filename in `ls *.py`
+for filename in $ALL
 do
 	echo "### python3 $filename ###"
 	python3 $filename
